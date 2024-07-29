1cae4d247312a7af0b5b5e871da6f669f8917692
tsolom01 <solomou.theodoros@gmail.com>
2024-07-29 11:46:53 +0200

    Initial Commit

---
+++ SunriseTravel/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  SunriseTravel
+//
+//  Created by user262355 on 7/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension ViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    func layout(){
+        
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
